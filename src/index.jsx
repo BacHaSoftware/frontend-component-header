9ed3c27505c7f805f15d9915e270693cc7845326
Bach Hung Cong <congbach.bhtech@gmail.com>
2024-06-19 09:50:41 +0700

    Update scss

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import LearningHeader from './learning-header/LearningHeader';
 import messages from './i18n/index';
 import StudioHeader from './studio-header';
 
+import './index.scss';
+
 export { LearningHeader, messages, StudioHeader };
 
 export default Header;
